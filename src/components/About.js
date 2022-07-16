6f2df8258f2329f516f3b52606617d2094e1e1ff
evillalba510 <evillalba510@gmail.com>
2022-07-15 22:44:40 -0500

    deploy

---
+++ src/components/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hola, soy Eliecer.
+            <br className="hidden lg:inline-block" />
+            Full Stack Developer Jr. Advanced
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            En mi recorrido de 15+ años por la industria IT como analista de
+            sistemas, agente de soporte, coordinador de sevicios, encargado de
+            control de calidad de software y ahora como desarrollador web full
+            stack; he aprendido el valor de trabajar en equipo, la proactividad,
+            el aprendizaje continuo, la autonomía en el trabajo, el compromiso y
+            la responsabilidad, no solo en el trabajo; sino también, en mi vida
+            personal. ¡Doy gracias a la vida por eso!
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Trabaja conmigo
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              Ver mis trabajos
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="https://avatars.githubusercontent.com/u/92558922?v=4"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    title: "Blue Bird Store",
+    subtitle: "e-commerce",
+    description: `Es una SPA de venta en linea de calzado. Entre sus características más notables están: búsqueda interactiva de productos, 
+    filtros por diversas calsificacicones, carta de ventas, favoritos, carrito de compras, pasarela de pagos, enví automátco de emails de estado 
+    del los pedidos, área administrativa de gestión de productos, variantes, pedidos, clasificaciones, usuarios, entre otros.`,
+    image: "https://media.giphy.com/media/qdMmoTuFqn24ixSagq/giphy.gif",
+    link: "https://bluebirdstore.vercel.app",
+  },
+];
+export const skills = [
+  "HTML / CSS / JavaScript",
+  "ReactJs",
+  "Redux",
+  "NodeJs",
+  "Express",
+  "SQL",
+  "PostgresSQL",
+  "Tailwind CSS",
+  "Postman",
+  "Jira",
+  "Confluence",
+  "Metodología agile SCRUM",
+];
+
+export const testimonials = [
+  {
+    name: "Pablo Lesta",
+    company: "Henry Bootcamp",
+    quote: `Eliecer es un gran compañero y lider nato , persona totalmente proactiva y comprometida con sus tareas .
+Exelente desempeño en el marco laboral y estudiantil . Aptitudes destacables tanto en lo tecnico como en el factor humano .`,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQH2VKK1UpcbSg/profile-displayphoto-shrink_400_400/0/1647973146087?e=1663200000&v=beta&t=6y6bGklwyiJOQ8M2-l0HfcTB9a-YeHmlbrUnmsQQZJM",
+  },
+];
